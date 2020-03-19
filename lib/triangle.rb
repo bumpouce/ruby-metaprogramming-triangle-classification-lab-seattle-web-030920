337602ab8488ab3e610f2f5bb3d0bdbd162b4422
christine <bumpouce@gmail.com>
2020-03-18 21:46:48 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+ 
+  def initialize (side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    is_triangle?
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side3 == side1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def is_triangle?
+    triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
+    [side1, side2, side3].each do |side|
+      begin
+        triangle << false if side <= 0
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+      raise TriangleError if triangle.include?(false)
+    end
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      "This is not a triangle!"
+    end
+  end
 end
+
